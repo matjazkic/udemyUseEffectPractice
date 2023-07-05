f8c40cfd9fcf8bf70afc45ed0e71857e956f8168
matjazkic <matjaz.kic@gmail.com>
2023-07-05 20:37:57 +0200

    added usecontext and restructured alot of code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useReducer } from "react";
-
+import React, { useState, useEffect, useReducer, useContext } from "react";
+import AuthContext from "../Context/auth-context";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
 const emailReducer = (state, action) => {
-  if (action.type === 'USER_INPUT') {
-    return { value: action.val, isValid: action.val.includes('@') };
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
   }
-  if(action.type === 'INPUT_BLUR')
+  if (action.type === "INPUT_BLUR")
     return { value: state.value, isValid: state.value.includes("@") };
-  
-  return { value:'', isValid: false}
+
+  return { value: "", isValid: false };
 };
 
 const passwordReducer = (state, action) => {
@@ -24,63 +24,65 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
-
 const Login = (props) => {
-/*   const [enteredEmail, setEnteredEmail] = useState("");
+  /*   const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState(); */
-/*   const [enteredPassword, setEnteredPassword] = useState("");
+  /*   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState(); */
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {value:'', isValid: false});
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: false })
-  
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
 
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log("effect running");
   }, []);
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
   // use effect example with timer(which here means nothing really, but with http request it makes sense to not send to many requests)
-  /* 
   useEffect(() => {
-   const identifier = setTimeout(() => { setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );console.log("checking"); }, 500)
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid && passwordIsValid);
+      console.log("checking");
+    }, 500);
+
     return () => {
-      console.log('cleanup')
-  clearTimeout(identifier)  };
-  }, [enteredEmail, enteredPassword]);
- */
+      console.log("cleanup");
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-   dispatchEmail({type: 'USER_INPUT', val: event.target.value})
-    setFormIsValid(
-      event.target.value.includes("@") && passwordState.value.trim().length > 6
-    );
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
   };
 
   const passwordChangeHandler = (event) => {
     /* setEnteredPassword(event.target.value); */
-    dispatchPassword({type: 'USER_INPUT', val: event.target.value})
-    setFormIsValid(
-      emailState.value.isValid && event.target.value.trim().length > 6
-    );
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
   };
 
   const validateEmailHandler = () => {
     /*  setEmailIsValid(emailState.isValid); */
-    dispatchEmail({type: 'INPUT_BLUR'})
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
     /*  setPasswordIsValid(enteredPassword.trim().length > 6); */
-    dispatchPassword({type: 'INPUT_BLUR'})
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
